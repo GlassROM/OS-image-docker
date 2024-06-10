4d4b8dfd0d80bd1ac9aaefec8d2784aef38343b0
randomhydrosol <randomhydrosol@glassrom.org>
2024-06-10 01:49:49 +0000

    fix build issues
    Signed-off-by: randomhydrosol <randomhydrosol@glassrom.org>

--- dockerprepare.sh
+++ dockerprepare.sh
@@ -2,12 +2,14 @@
 TAG=$(openssl rand -base64 128 | tr -dc 'a-z')
 echo "tagging as $TAG"
 echo $TAG $(date) | tee -a ./archinstall/container-id
-rm ./archlinux/etc/pacman.d/gnupg -rvf
+rm ./archinstall/etc/pacman.d/gnupg -rvf
 rm ./archinstall/etc/machine-id -rvf
 rm ./archinstall/root/.bash_history -vf
 
 cp ../seccomp-mdwe/seccomp-error ./archinstall -av
 cp ../seccomp-mdwe/seccomp-strict ./archinstall -av
+
+reflector --latest 50 --age 24 --protocol https --save ./archinstall/etc/pacman.d/mirrorlist
 
 find ./archinstall -perm /4000 -type f -exec chmod u-s {} \;
 find ./archinstall -perm /2000 -type f -exec chmod g-s {} \;
