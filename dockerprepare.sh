fb686671e49b528ab7c8218121d8c39605bc2cc7
randomhydrosol <randomhydrosol@glassrom.org>
2024-06-27 03:02:33 +0000

    add support to sign the images
    Signed-off-by: randomhydrosol <randomhydrosol@glassrom.org>

--- dockerprepare.sh
+++ dockerprepare.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+echo "Please enter your GitHub personal access token: "
+read -s TOKEN
 TAG=$(openssl rand -base64 128 | tr -dc 'a-z')
 echo "tagging as $TAG"
 echo $TAG $(date) | tee -a ./archinstall/container-id
@@ -17,4 +19,8 @@ find ./archinstall -perm /2000 -type f -exec chmod g-s {} \;
 ./dockersafebuild -t $TAG;
 
 echo FROM $TAG > TAGINFO
+echo $TOKEN | docker login ghcr.io -u randomhydrosol --password-stdin
+docker tag $TAG ghcr.io/glassrom/os-image-docker:latest
+docker push ghcr.io/glassrom/os-image-docker:latest
+cosign sign ghcr.io/glassrom/os-image-docker:latest
 reboot
